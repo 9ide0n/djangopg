2e443df0ca0193cd873dec2480e089300f2c59f4
gide0n <->
2017-11-24 22:03:33 +0300

    Remove comments

--- djangopg/polls/views.py
+++ djangopg/polls/views.py
@@ -9,26 +9,12 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
-    #    output = ', '.join([q.question_text for q in latest_question_list])
-    #    return HttpResponse(output)
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
-    # or so without using render shortcuts
-    #
-    # template = loader.get_template('polls/index.html')
-    # context = {'latest_question_list': latest_question_list}
-    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    #      or so without using get_object_or_404 shortcut
-    #    try:
-    #        question = Question.objects.get(pk=question_id)
-    #    except Question.DoesNotExist:
-    #        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 
